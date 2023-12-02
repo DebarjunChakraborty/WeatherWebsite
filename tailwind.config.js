b04e537e237a3f3c21aa10142ff171fd51889f90
Debarjun Chakraborty <147123894+DebarjunChakraborty@users.noreply.github.com>
2023-12-02 13:40:15 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        primaryColor: '#FFFFFF',
+        secondoryColor: '#47C28C',
+        pageColor: '#F3F8FA',
+        btncolor: '#174F23',
+        hoverColor:'#f5b547',
+        footerColor:'#C9AA67',
+        footerText:'#DCCD98',
+      }
+    },
+  },
+  plugins: [],
+}
+
